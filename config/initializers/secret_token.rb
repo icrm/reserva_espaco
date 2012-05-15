1848584046d22c4e710f41b4ad9454b0b017ec39
Reinaldo Oliveira <reinaldooli@gmail.com>
2012-05-15 16:08:13 -0300

    Criando o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReservaEspaco::Application.config.secret_token = '760b376d1ec35dc06e295706d669fee60ba92d314070bbfbd6242009f126f05cb49f83285d722ba5796fa6f45a46422120e11dae4a9518f2b03faa7e12750927'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ReservaEspaco::Application.config.session_store :cookie_store, key: '_reserva_espaco_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ReservaEspaco::Application.config.session_store :active_record_store
